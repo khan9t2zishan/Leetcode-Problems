4d291509720aeea28667a93227e6d0c10053ea67
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2023-12-20 22:52:34 +0530

    Buy Two Chocolates
    You are given an integer array prices representing the prices of various chocolates in a store. You are also given a single integer money, which represents your initial amount of money.
    
    You must buy exactly two chocolates in such a way that you still have some non-negative leftover money. You would like to minimize the sum of the prices of the two chocolates you buy.
    
    Return the amount of money you will have leftover after buying the two chocolates. If there is no way for you to buy two chocolates without ending up in debt, return money. Note that the leftover must be non-negative.
    
     
    
    Example 1:
    
    Input: prices = [1,2,2], money = 3
    Output: 0
    Explanation: Purchase the chocolates priced at 1 and 2 units respectively. You will have 3 - 3 = 0 units of money afterwards. Thus, we return 0.
    Example 2:
    
    Input: prices = [3,2,3], money = 3
    Output: 3
    Explanation: You cannot buy 2 chocolates without going in debt, so we return 3.
     
    
    Constraints:
    
    2 <= prices.length <= 50
    1 <= prices[i] <= 100
    1 <= money <= 100

---
+++ Easy/2706- Buy Two Chocolates.cpp
@@ -0,0 +1,29 @@
+/*
+
+Author : Zishan Khan
+Problem : Buy Two Chocolates
+Difficulty : Easy
+Problem Link : https://leetcode.com/problems/buy-two-chocolates/description/?envType=daily-question&envId=2023-12-20
+
+*/
+
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int firstMinCost = INT_MAX;
+        int secondMinCost = INT_MAX;
+
+        for (int p : prices) {
+            if (p < firstMinCost) {
+                secondMinCost = firstMinCost;
+                firstMinCost = p;
+            } else {
+                secondMinCost = min(secondMinCost, p);
+            }
+        }
+
+        int leftover = money - (firstMinCost + secondMinCost);
+
+        return leftover >= 0 ? leftover : money;        
+    }
+};
