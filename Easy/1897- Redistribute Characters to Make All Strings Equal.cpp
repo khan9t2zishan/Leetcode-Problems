d98de5440c83f48523dd76ab347fcf5c8db95ad9
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2023-12-30 21:16:17 +0530

    Redistribute Characters to Make All Strings Equal
    You are given an array of strings words (0-indexed).
    
    In one operation, pick two distinct indices i and j, where words[i] is a non-empty string, and move any character from words[i] to any position in words[j].
    
    Return true if you can make every string in words equal using any number of operations, and false otherwise.
    
     
    
    Example 1:
    
    Input: words = ["abc","aabc","bc"]
    Output: true
    Explanation: Move the first 'a' in words[1] to the front of words[2],
    to make words[1] = "abc" and words[2] = "abc".
    All the strings are now equal to "abc", so return true.
    Example 2:
    
    Input: words = ["ab","a"]
    Output: false
    Explanation: It is impossible to make all the strings equal using the operation.
     
    
    Constraints:
    
    1 <= words.length <= 100
    1 <= words[i].length <= 100
    words[i] consists of lowercase English letters.

---
+++ Easy/1897- Redistribute Characters to Make All Strings Equal.cpp
@@ -0,0 +1,41 @@
+/*
+
+Author : Zishan Khan
+Problem : Redistribute Characters to Make All Strings Equal
+Difficulty : Easy
+Problem Link : https://leetcode.com/problems/redistribute-characters-to-make-all-strings-equal/description/?envType=daily-question&envId=2023-12-30
+
+*/
+
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        if (words.size() == 1) {
+            return true;
+        }
+
+        int totalCharCount = 0;
+        for (const string& s : words) {
+            totalCharCount += s.length();
+        }
+
+        if (totalCharCount % words.size() != 0) {
+            return false;
+        }
+
+        vector<int> myMap(26, 0);
+        for (const string& s : words) {
+            for (char c : s) {
+                myMap[c - 'a']++;
+            }
+        }
+
+        for (int i : myMap) {
+            if (i % words.size() != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
