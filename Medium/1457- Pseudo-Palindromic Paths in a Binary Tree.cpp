e190b5ba43a3745dffc724067311b692af35ba51
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-01-24 22:11:38 +0530

    Pseudo-Palindromic Paths in a Binary Tree
    Given a binary tree where node values are digits from 1 to 9. A path in the binary tree is said to be pseudo-palindromic if at least one permutation of the node values in the path is a palindrome.
    
    Return the number of pseudo-palindromic paths going from the root node to leaf nodes.
    
     
    
    Example 1:
    
    
    
    Input: root = [2,3,1,3,1,null,1]
    Output: 2 
    Explanation: The figure above represents the given binary tree. There are three paths going from the root node to leaf nodes: the red path [2,3,3], the green path [2,1,1], and the path [2,3,1]. Among these paths only red path and green path are pseudo-palindromic paths since the red path [2,3,3] can be rearranged in [3,2,3] (palindrome) and the green path [2,1,1] can be rearranged in [1,2,1] (palindrome).
    Example 2:
    
    
    
    Input: root = [2,1,1,1,3,null,null,null,null,null,1]
    Output: 1 
    Explanation: The figure above represents the given binary tree. There are three paths going from the root node to leaf nodes: the green path [2,1,1], the path [2,1,3,1], and the path [2,1]. Among these paths only the green path is pseudo-palindromic since [2,1,1] can be rearranged in [1,2,1] (palindrome).
    Example 3:
    
    Input: root = [9]
    Output: 1
     
    
    Constraints:
    
    The number of nodes in the tree is in the range [1, 10^5].
    1 <= Node.val <= 9

---
+++ Medium/1457- Pseudo-Palindromic Paths in a Binary Tree.cpp
@@ -0,0 +1,58 @@
+/*
+
+Author : Zishan Khan
+Problem : Pseudo-Palindromic Paths in a Binary Tree
+Difficulty : Medium
+Problem Link : https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/description/?envType=daily-question&envId=2024-01-24
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+
+class Solution {
+public:
+    int pseudoPalindromicPaths(TreeNode* root) {
+        int count = 0, path = 0;
+
+        stack<pair<TreeNode*, int>> stk;
+        stk.push({root, 0});
+
+        while (!stk.empty()) {
+            auto [node, path] = stk.top();
+            stk.pop();
+
+            if (node != nullptr) {
+                
+                path = path ^ (1 << node->val);
+
+                
+                if (node->left == nullptr && node->right == nullptr) {
+                    
+                    if ((path & (path - 1)) == 0) {
+                        ++count;
+                    }
+                } else {
+                    stk.push({node->right, path});
+                    stk.push({node->left, path});
+                }
+            }
+        }
+
+        return count;
+    }
+};
+
+
+
