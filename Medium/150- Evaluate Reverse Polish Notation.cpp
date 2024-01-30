955111e3340caf25f84f7a75b8a048aefeba7cee
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-01-30 23:34:51 +0530

    Evaluate Reverse Polish Notation
    You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
    
    Evaluate the expression. Return an integer that represents the value of the expression.
    
    Note that:
    
    The valid operators are '+', '-', '*', and '/'.
    Each operand may be an integer or another expression.
    The division between two integers always truncates toward zero.
    There will not be any division by zero.
    The input represents a valid arithmetic expression in a reverse polish notation.
    The answer and all the intermediate calculations can be represented in a 32-bit integer.
     
    
    Example 1:
    
    Input: tokens = ["2","1","+","3","*"]
    Output: 9
    Explanation: ((2 + 1) * 3) = 9
    Example 2:
    
    Input: tokens = ["4","13","5","/","+"]
    Output: 6
    Explanation: (4 + (13 / 5)) = 6
    Example 3:
    
    Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
    Output: 22
    Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
    = ((10 * (6 / (12 * -11))) + 17) + 5
    = ((10 * (6 / -132)) + 17) + 5
    = ((10 * 0) + 17) + 5
    = (0 + 17) + 5
    = 17 + 5
    = 22
     
    
    Constraints:
    
    1 <= tokens.length <= 10^4
    tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].

---
+++ Medium/150- Evaluate Reverse Polish Notation.cpp
@@ -0,0 +1,40 @@
+/*
+
+Author : Zishan Khan
+Problem : Evaluate Reverse Polish Notation
+Difficulty : Medium
+Problem Link : https://leetcode.com/problems/evaluate-reverse-polish-notation/description/?envType=daily-question&envId=2024-01-30
+
+*/
+
+
+class Solution {
+
+    long resolves(int a, int b, char Operator){
+        if(Operator == '+') return a + b;
+        else if(Operator == '-') return a - b;
+        else if(Operator == '*') return (long)a*b;
+        return a/b;
+    }
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<long> Stack;
+        int n = tokens.size();
+        for(int i = 0; i < n; i++){
+
+            if(tokens[i].size() == 1 and tokens[i][0] < 48){
+                long integer2 = Stack.top();
+                Stack.pop();
+                long integer1 = Stack.top();
+                Stack.pop();
+                
+                string Operator = tokens[i];
+                long resolvedAns = resolves(integer1, integer2 , Operator[0]);
+                Stack.push(resolvedAns);
+            }else 
+                Stack.push(stol(tokens[i]));
+        }
+        return Stack.top();
+    }
+};
+
