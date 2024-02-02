3ad99d63e386c2fb3aa7da08ffded6b0706064e6
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-02-02 21:45:53 +0530

    Sequential Digits
    An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
    
    Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
    
     
    
    Example 1:
    
    Input: low = 100, high = 300
    Output: [123,234]
    Example 2:
    
    Input: low = 1000, high = 13000
    Output: [1234,2345,3456,4567,5678,6789,12345]
     
    
    Constraints:
    
    10 <= low <= high <= 10^9

---
+++ Medium/1291- Sequential Digits.cpp
@@ -0,0 +1,31 @@
+/*
+
+Author : Zishan Khan
+Problem : Sequential Digits
+Difficulty : Medium
+Problem Link : https://leetcode.com/problems/sequential-digits/description/?envType=daily-question&envId=2024-02-02
+
+*/
+
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int> a;
+
+        for (int i = 1; i <= 9; ++i) {
+            int num = i;
+            int nextDigit = i + 1;
+
+            while (num <= high && nextDigit <= 9) {
+                num = num * 10 + nextDigit;
+                if (low <= num && num <= high) a.push_back(num);
+                ++nextDigit;
+            }
+        }
+
+        sort(a.begin(), a.end());
+        return a;
+    }
+};
+
+
