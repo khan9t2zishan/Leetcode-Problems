9257255535abd23fdc289f7f25b8c5107a5a835c
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-06-07 20:49:27 +0530

    Hand of Straights
    Alice has some number of cards and she wants to rearrange the cards into groups so that each group is of size groupSize, and consists of groupSize consecutive cards.
    
    Given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize, return true if she can rearrange the cards, or false otherwise.
    
     
    
    Example 1:
    
    Input: hand = [1,2,3,6,2,3,4,7,8], groupSize = 3
    Output: true
    Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8]
    Example 2:
    
    Input: hand = [1,2,3,4,5], groupSize = 4
    Output: false
    Explanation: Alice's hand can not be rearranged into groups of 4.
    
     
    
    Constraints:
    
    1 <= hand.length <= 10^4
    0 <= hand[i] <= 10^9
    1 <= groupSize <= hand.length

---
+++ Medium/846- Hand of Straights.cpp
@@ -0,0 +1,39 @@
+/*
+
+Author : Zishan Khan
+Problem : Hand of Straights
+Difficulty : Medium
+Problem Link : https://leetcode.com/problems/hand-of-straights/description/?envType=daily-question&envId=2024-06-06
+
+*/
+
+class Solution {
+public:
+   bool findSuccessors(vector<int>& hand, int groupSize, int i, int n) {
+        int next = hand[i] + 1;
+        hand[i] = -1;  // Mark as used
+        int count = 1;
+        i += 1;
+        while (i < n && count < groupSize) {
+            if (hand[i] == next) {
+                next = hand[i] + 1;
+                hand[i] = -1;
+                count++;
+            }
+            i++;
+        }
+        return count == groupSize;
+    }
+
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+        if (n % groupSize != 0) return false;
+        std::sort(hand.begin(), hand.end());
+        for (int i = 0; i < n; i++) {
+            if (hand[i] >= 0) {
+                if (!findSuccessors(hand, groupSize, i, n)) return false;
+            }
+        }
+        return true;
+    }
+};
