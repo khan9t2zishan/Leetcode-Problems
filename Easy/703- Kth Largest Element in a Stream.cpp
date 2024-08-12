f4f6d1de8549ebe828d8284cb47dd8f08fe1252e
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-08-12 19:43:11 +0530

    Kth Largest Element in a Stream
    Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
    
    Implement KthLargest class:
    
    KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
    int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
     
    
    Example 1:
    
    Input
    ["KthLargest", "add", "add", "add", "add", "add"]
    [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
    Output
    [null, 4, 5, 5, 8, 8]
    
    Explanation
    KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
    kthLargest.add(3);   // return 4
    kthLargest.add(5);   // return 5
    kthLargest.add(10);  // return 5
    kthLargest.add(9);   // return 8
    kthLargest.add(4);   // return 8
     
    
    Constraints:
    
    1 <= k <= 10^4
    0 <= nums.length <= 10^4
    -10^4 <= nums[i] <= 10^4
    -10^4 <= val <= 10^4
    At most 104 calls will be made to add.
    It is guaranteed that there will be at least k elements in the array when you search for the kth element.

---
+++ Easy/703- Kth Largest Element in a Stream.cpp
@@ -0,0 +1,38 @@
+/*
+
+Author : Zishan Khan
+Problem : Kth Largest Element in a Stream
+Difficulty : Easy
+Problem Link : https://leetcode.com/problems/kth-largest-element-in-a-stream/description/?envType=daily-question&envId=2024-08-12
+
+*/
+
+class KthLargest {
+public:
+    KthLargest(int k, vector<int>& nums) : k(k) {
+        for (int num : nums) {
+            if (minHeap.size() < k) {
+                minHeap.push(num);
+            } else if (num > minHeap.top()) {
+                minHeap.push(num);
+                if (minHeap.size() > k) {
+                    minHeap.pop();
+                }
+            }
+        }
+    }
+
+    int add(int val) {
+        if (minHeap.size() < k) {
+            minHeap.push(val);
+        } else if (val > minHeap.top()) {
+            minHeap.push(val);
+            minHeap.pop();
+        }
+        return minHeap.top();
+    }
+
+private:
+    int k;
+    priority_queue<int, vector<int>, greater<int>> minHeap;  // Min-heap to store the k largest elements
+};
