f506d635433d63f72ce90452d4f207f95a8dd936
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-08-15 21:02:09 +0530

    Lemonade Change
    At a lemonade stand, each lemonade costs $5. Customers are standing in a queue to buy from you and order one at a time (in the order specified by bills). Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill. You must provide the correct change to each customer so that the net transaction is that the customer pays $5.
    
    Note that you do not have any change in hand at first.
    
    Given an integer array bills where bills[i] is the bill the ith customer pays, return true if you can provide every customer with the correct change, or false otherwise.
    
     
    
    Example 1:
    
    Input: bills = [5,5,5,10,20]
    Output: true
    Explanation: 
    From the first 3 customers, we collect three $5 bills in order.
    From the fourth customer, we collect a $10 bill and give back a $5.
    From the fifth customer, we give a $10 bill and a $5 bill.
    Since all customers got correct change, we output true.
    Example 2:
    
    Input: bills = [5,5,10,10,20]
    Output: false
    Explanation: 
    From the first two customers in order, we collect two $5 bills.
    For the next two customers in order, we collect a $10 bill and give back a $5 bill.
    For the last customer, we can not give the change of $15 back because we only have two $10 bills.
    Since not every customer received the correct change, the answer is false.
     
    
    Constraints:
    
    1 <= bills.length <= 10^5
    bills[i] is either 5, 10, or 20.

---
+++ Easy/860- Lemonade Change.cpp
@@ -0,0 +1,39 @@
+/*
+
+Author : Zishan Khan
+Problem : Lemonade Change
+Difficulty : Easy
+Problem Link : https://leetcode.com/problems/lemonade-change/description/?envType=daily-question&envId=2024-08-15
+
+*/
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five_dollars = 0, ten_dollars = 0;
+        
+        for (int x : bills) {
+            if (x == 5) {
+                five_dollars++;
+            } else if (x == 10) {
+                if (five_dollars > 0) {
+                    five_dollars--;
+                    ten_dollars++;
+                } else {
+                    return false;
+                }
+            } else {
+                if (five_dollars > 0 && ten_dollars > 0) {
+                    five_dollars--;
+                    ten_dollars--;
+                } else if (five_dollars > 2) {
+                    five_dollars -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
