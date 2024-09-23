d2df6977555fbf34eefbca51f4e7310c6893b235
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-09-23 22:00:39 +0530

    Extra Characters in a String
    You are given a 0-indexed string s and a dictionary of words dictionary. You have to break s into one or more non-overlapping substrings such that each substring is present in dictionary. There may be some extra characters in s which are not present in any of the substrings.
    
    Return the minimum number of extra characters left over if you break up s optimally.
    
     
    
    Example 1:
    
    Input: s = "leetscode", dictionary = ["leet","code","leetcode"]
    Output: 1
    Explanation: We can break s in two substrings: "leet" from index 0 to 3 and "code" from index 5 to 8. There is only 1 unused character (at index 4), so we return 1.
    
    Example 2:
    
    Input: s = "sayhelloworld", dictionary = ["hello","world"]
    Output: 3
    Explanation: We can break s in two substrings: "hello" from index 3 to 7 and "world" from index 8 to 12. The characters at indices 0, 1, 2 are not used in any substring and thus are considered as extra characters. Hence, we return 3.
     
    
    Constraints:
    
    1 <= s.length <= 50
    1 <= dictionary.length <= 50
    1 <= dictionary[i].length <= 50
    dictionary[i] and s consists of only lowercase English letters
    dictionary contains distinct words

---
+++ Medium/2707- Extra Characters in a String.cpp
@@ -0,0 +1,30 @@
+/*
+
+Author : Zishan Khan
+Problem : Extra Characters in a String
+Difficulty : Medium
+Problem Link : https://leetcode.com/problems/extra-characters-in-a-string/description/?envType=daily-question&envId=2024-09-23
+
+*/
+
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        unordered_set<string> dictSet;
+        int sz = s.size();
+        for(auto word : dictionary){
+            dictSet.insert(word);
+        }
+        vector<int> delCount(sz+1);
+        for(int right = 1; right<=sz; right++){
+                delCount[right] = 1 + delCount[right-1];
+                for(int left = right; left > 0; left--){
+                    string word = s.substr(left-1, right - left+1);
+                    if(dictSet.count(word)){
+                        delCount[right] = min(delCount[right], delCount[left-1]);
+                    }
+                }
+        }
+        return delCount[sz];
+    }
+};
