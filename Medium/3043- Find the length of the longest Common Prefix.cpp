0c231090402a2bdb1c57aceb096677a7ea83da11
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-09-25 20:47:35 +0530

    Find the length of the longest Common Prefix
    You are given two arrays with positive integers arr1 and arr2.
    
    A prefix of a positive integer is an integer formed by one or more of its digits, starting from its leftmost digit. For example, 123 is a prefix of the integer 12345, while 234 is not.
    
    A common prefix of two integers a and b is an integer c, such that c is a prefix of both a and b. For example, 5655359 and 56554 have a common prefix 565 while 1223 and 43456 do not have a common prefix.
    
    You need to find the length of the longest common prefix between all pairs of integers (x, y) such that x belongs to arr1 and y belongs to arr2.
    
    Return the length of the longest common prefix among all pairs. If no common prefix exists among them, return 0.
    
     
    
    Example 1:
    
    Input: arr1 = [1,10,100], arr2 = [1000]
    Output: 3
    Explanation: There are 3 pairs (arr1[i], arr2[j]):
    - The longest common prefix of (1, 1000) is 1.
    - The longest common prefix of (10, 1000) is 10.
    - The longest common prefix of (100, 1000) is 100.
    The longest common prefix is 100 with a length of 3.
    Example 2:
    
    Input: arr1 = [1,2,3], arr2 = [4,4,4]
    Output: 0
    Explanation: There exists no common prefix for any pair (arr1[i], arr2[j]), hence we return 0.
    Note that common prefixes between elements of the same array do not count.

--- Medium/3043- Find the length of the longest Common Prefix.cpp
+++ Medium/3043- Find the length of the longest Common Prefix.cpp
@@ -1,1 +1,44 @@
+/*
 
+Author : Zishan Khan
+Problem : Find the Length of the Longest Common Prefix
+Difficulty : Medium
+Problem Link : https://leetcode.com/problems/find-the-length-of-the-longest-common-prefix/description/?envType=daily-question&envId=2024-09-24
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        unordered_map<string, int> prefixMap;
+        
+        // Step 1: Build the prefix map for arr1
+        for (int num : arr1) {
+            string strNum = to_string(num);
+            string prefix = "";
+            for (char ch : strNum) {
+                prefix += ch;
+                prefixMap[prefix]++;
+            }
+        }
+        
+        int maxLength = 0;
+        
+        // Step 2: Check for common prefixes in arr2
+        for (int num : arr2) {
+            string strNum = to_string(num);
+            string prefix = "";
+            for (char ch : strNum) {
+                prefix += ch;
+                if (prefixMap.find(prefix) != prefixMap.end()) {
+                    maxLength = max(maxLength, static_cast<int>(prefix.length()));
+                }
+            }
+        }
+        
+        return maxLength;
+    }
+};
