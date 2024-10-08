3ea593681f3d86487ef1f8720bf4ed17817eb6f7
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-10-08 06:45:41 +0530

    Minimum Number of Swaps to Make the String Balanced
    You are given a 0-indexed string s of even length n. The string consists of exactly n / 2 opening brackets '[' and n / 2 closing brackets ']'.
    
    A string is called balanced if and only if:
    
    It is the empty string, or
    It can be written as AB, where both A and B are balanced strings, or
    It can be written as [C], where C is a balanced string.
    You may swap the brackets at any two indices any number of times.
    
    Return the minimum number of swaps to make s balanced.
    
     
    
    Example 1:
    
    Input: s = "][]["
    Output: 1
    Explanation: You can make the string balanced by swapping index 0 with index 3.
    The resulting string is "[[]]".
    Example 2:
    
    Input: s = "]]][[["
    Output: 2
    Explanation: You can do the following to make the string balanced:
    - Swap index 0 with index 4. s = "[]][][".
    - Swap index 1 with index 5. s = "[[][]]".
    The resulting string is "[[][]]".
    Example 3:
    
    Input: s = "[]"
    Output: 0
    Explanation: The string is already balanced.
     
    
    Constraints:
    
    n == s.length
    2 <= n <= 10^6
    n is even.
    s[i] is either '[' or ']'.
    The number of opening brackets '[' equals n / 2, and the number of closing brackets ']' equals n / 2.

--- Medium/1963- Minimum Number of Swaps to Make the String Balanced.cpp
+++ Medium/1963- Minimum Number of Swaps to Make the String Balanced.cpp
@@ -1,1 +1,25 @@
+/*
 
+Author : Zishan Khan
+Problem : Minimum Number of Swaps to Make the String Balanced
+Difficulty : Medium
+Problem Link : https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/description/?envType=daily-question&envId=2024-10-08
+
+*/
+
+class Solution {
+public:
+    int minSwaps(string s) {
+        int size = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            if (ch == '[')
+                size++;
+            else if (size > 0) 
+                size--;
+            
+        }
+        return (size + 1)/2;
+    }
+};
