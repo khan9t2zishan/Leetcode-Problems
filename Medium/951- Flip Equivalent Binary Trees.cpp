52ea90df1723ee95fbba2b65bba3d96ec0902c3d
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-10-24 18:44:48 +0530

    Flip Equivalent Binary Trees
    For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
    
    A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
    
    Given the roots of two binary trees root1 and root2, return true if the two trees are flip equivalent or false otherwise.
    
     
    
    Example 1:
    
    Flipped Trees Diagram
    Input: root1 = [1,2,3,4,5,6,null,null,null,7,8], root2 = [1,3,2,null,6,4,5,null,null,null,null,8,7]
    Output: true
    Explanation: We flipped at nodes with values 1, 3, and 5.
    Example 2:
    
    Input: root1 = [], root2 = []
    Output: true
    Example 3:
    
    Input: root1 = [], root2 = [1]
    Output: false
     
    
    Constraints:
    
    The number of nodes in each tree is in the range [0, 100].
    Each tree will have unique node values in the range [0, 99].

---
+++ Medium/951- Flip Equivalent Binary Trees.cpp
@@ -0,0 +1,29 @@
+/*
+
+Author : Zishan Khan
+Problem : Flip Equivalent Binary Trees
+Difficulty : Medium
+Problem Link : https://leetcode.com/problems/flip-equivalent-binary-trees/description/?envType=daily-question&envId=2024-10-24
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+         if (root1 == root2) return 1;
+        if (!root1 || !root2 || root1->val != root2->val) return 0;
+        return (flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right)) ||
+               (flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left));
+    }
+};
