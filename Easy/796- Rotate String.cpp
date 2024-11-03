4be2178326273f1f1a39f2dfbe0f14503d5c5eb5
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2024-11-03 20:26:21 +0530

    Rotate String
    Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
    
    A shift on s consists of moving the leftmost character of s to the rightmost position.
    
    For example, if s = "abcde", then it will be "bcdea" after one shift.
     
    
    Example 1:
    
    Input: s = "abcde", goal = "cdeab"
    Output: true
    Example 2:
    
    Input: s = "abcde", goal = "abced"
    Output: false
     
    
    Constraints:
    
    1 <= s.length, goal.length <= 100
    s and goal consist of lowercase English letters.

--- Easy/796- Rotate String.cpp
+++ Easy/796- Rotate String.cpp
@@ -1,1 +1,18 @@
+/*
 
+Author : Zishan Khan
+Problem : Rotate String
+Difficulty : Easy
+Problem Link : https://leetcode.com/problems/rotate-string/description/?envType=daily-question&envId=2024-11-03
+
+*/
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        return (s + s).find(goal) != string::npos;
+    }
+};
